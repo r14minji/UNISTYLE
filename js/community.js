db58c61eadc1c33a71b9dab57bbb31036ab7ed16
r14minji <rkdalswl0728@gmail.com>
2021-12-12 14:49:40 +0900

    step8 community section/tab/FAQ 탭 js

--- js/community.js
+++ js/community.js
@@ -1,3 +1,32 @@
+//tab
+const tab = document.querySelector("#tab_community");
+const dts = tab.querySelectorAll("dt");
+const dds = tab.querySelectorAll("dd");
+const dt_btn = document.querySelectorAll("dt>a");
+
+dts.forEach((el, index) =>{
+  el.addEventListener("click", e => {
+    e.preventDefault();
+    let isOn = e.currentTarget.classList.contains("on");
+    if(isOn) return;
+    activationTab(dts, index);
+    activationTab(dds, index);
+  })
+})
+
+dt_btn.forEach((el, index) => {
+  el.addEventListener("focusin", e => {
+    activation(dts, index);
+    activation(dds, index);
+  })
+})
+
+function activationTab(items, index){
+  for(let item of items){
+    item.classList.remove("on");
+  }
+  items[index].classList.add("on");
+}
 
 //accordion
 const boardFAQ = document.querySelector("#FAQ");
