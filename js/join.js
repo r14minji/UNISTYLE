df8523b8b5f2b0f0647ebfa91c830c31e1d9da63
r14minji <rkdalswl0728@gmail.com>
2021-12-13 15:22:52 +0900

    step10 join js isTxt 함수 만들었지만 오류

--- js/join.js
+++ js/join.js
@@ -2,22 +2,40 @@ const form = document.querySelector("#member");
 const submitBtn = form.querySelector("input[type=submit]");
 
 submitBtn.addEventListener("click", e => {
+  e.preventDefault();
+  if(!isTxt("fisrtName", 1, "First Name")) e.preventDefault();
+  if(!isTxt("lastName", 1, "Last Name")) e.preventDefault();
+  if(!isTxt("userid", 6, "ID using 6 or more characters")) e.preventDefault();
 
 })
 
-function isTxt(name, len){
+//text input 인증 함수 
+function isTxt(name, len, comment){
   
-  if(len == undefined) len = 5;
+  if(len == undefined) len = 1;
 
-  let input = form.querySelector("input[type=text]");
+  let input = form.querySelector(`[name=${name}]`);
+  console.log(input);
   let txt = input.value;
+  console.log(txt);
 
-  if( txt.length != '' && txt.length >= len){
+  if((txt.length >= len && txt !="")){
+    const errMsgs = form.querySelectorAll("p");
+    if(errMsgs.length > 0) input.closest("td").querySelector("p").remove();
 
+    return true;
   }else{
-    const errMsgs = input.closest("td")
+    const errMsgs = form.querySelectorAll("p");
+    if(errMsgs.length > 0) input.closest("td").querySelector("p").remove();
 
     const errMsg = document.createElement("p");
-    errMsg.append(`Please enter a valid ${} (more then ${len})`)
+    errMsg.append(`Please enter your ${comment}`);
+    input.closest("td").append(errMsg);
+    return false;
   }
+}
+
+//password 인증함수
+function isPwd(){
+  const pwd1 = form.querySelector(`[name=${"pwd1"}]`)
 }
