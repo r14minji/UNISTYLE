925e98a5c6a860e4d40bdfc48b424ffdfd15d2a7
r14minji <rkdalswl0728@gmail.com>
2021-12-14 22:02:15 +0900

    step4 index/header 2depth js mouse enter

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const btnCall = document.querySelector(".btnCall");
 const menuMo = document.querySelector(".menuMo");
+const gnb_lis = document.querySelectorAll(".gnbWrap>ul>li")
 
 //반응형 메뉴버튼
 btnCall.addEventListener("click", e =>{
@@ -9,3 +10,17 @@ btnCall.addEventListener("click", e =>{
   menuMo.classList.toggle("on");
 })
 
+//2depth 
+gnb_lis.forEach( li=>{
+  li.addEventListener("mouseenter", e=>{
+
+    const sub = e.currentTarget.querySelector(".sub");
+    if(sub !=null){
+      sub.style.display = "block";
+    }
+
+
+    const depth1 = e.currentTarget.querySelector("a");
+    depth1.classList.add("on");
+  })
+})
