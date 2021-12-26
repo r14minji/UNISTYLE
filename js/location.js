1856ada8904191b1be03fa7c3be879321e5b9e7c
r14minji <rkdalswl0728@gmail.com>
2021-12-26 15:37:22 +0900

    location 리사이즈 오류 수정

--- js/location.js
+++ js/location.js
@@ -61,11 +61,10 @@ for (let i = 0; i < positions.length; i ++) {
 
 //리사이즈 (반응형 웹 필요)
 window.onresize = () => {
-  let active_btn = document.querySelector("dt.on");
+  let active_btn = document.querySelector(".branch li.on");
   let active_index = active_btn.getAttribute("data-index");
   map.setCenter(positions[active_index].latlng)
 }
-
 
 //지도이동시키기 함수 (중요)
 setCenter(positions[0].latlng);
