314a9d3ff34888f9d8ab41dccc0f72cb1f85d213
r14minji <rkdalswl0728@gmail.com>
2021-12-26 12:09:08 +0900

    youtube popup close버튼 설정

--- js/youtube.js
+++ js/youtube.js
@@ -1,27 +1,37 @@
-const vidList = document.querySelector("#youtubeVidList");
-const key = "AIzaSyCB33AAQUtZKdJsxck78NerCW1I2nv_yYo";
+const body = document.querySelector("body");
+const vidList = document.querySelector(".vidList");
+const key = "AIzaSyB3Xi97H8RT0bj6sAR6FQRG1TB8ts5Br7k";
 const playListId = "PL5jd_nA7BbYsJhoye24NUYMlS6eUq1v1a";
-const num = 6;
+const num = 8;
 const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playListId}&maxResult=${num}`;
+
 
 fetch(url)
 .then(data => {
   return data.json();
 })
 .then(json => {
-  //console.log(json.items);
+  console.log(json.items);
   let items = json.items;
 
   let result = "";
+  let count = 0;
+
   items.forEach(item =>{
     let title = item.snippet.title;
+    if(title.length >32) {
+      title = title.substr(0,32) + '...';
+    }
 
     let con = item.snippet.description;
+    if(con.length > 80){
+      con =  con.substr(0, 80);
+    }
 
     let date = item.snippet.publishedAt;
     date = date.split("T")[0];
 
-    let count = 0;
+    count = count++; 
 
     result += `
     <article>
@@ -42,4 +52,49 @@ fetch(url)
     `
   })
   vidList.innerHTML = result;
+})
+
+
+//팝업생성
+vidList.addEventListener("click", e => {
+  e.preventDefault();
+
+  if(e.target.closest("a") == null) return;
+  const vidId = e.target.closest("a").getAttribute("href");
+  const count = e.target.closest("article").querySelector("strong").innerText;  
+  const title = e.target.closest("article").querySelector("h2").innerText;  
+  const des = e.target.closest("article").querySelector("p").innerText;  
+  
+  const pop = document.createElement("aside");
+  pop.classList.add("popup_youtube");
+  
+  body.style.overflow = "hidden";
+
+  pop.innerHTML = `
+  <div class="inner">
+    <div class="con">
+      <strong>${count}</strong>
+      <h1>${title}</h1>
+      <p>${des}</p>
+    </div>
+    <iframe src="https://www.youtube.com/embed/${vidId}" frameborder="0" height="100%" allowfullscreen ></iframe>
+    <span>Close</span>
+  </div>
+  `;
+
+  body.append(pop);
+})
+
+
+//팝업 닫기
+body.addEventListener("click", e=> {
+  const pop = e.currentTarget.querySelector("aside");
+
+  if(pop != null) {
+    const close = pop.querySelector("span");
+    if(e.target == close){
+      pop.remove();
+      body.style.overflow = "visible";
+    }
+  }
 })
