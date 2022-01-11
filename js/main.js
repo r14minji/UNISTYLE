3a0cf438ead5f580b0f38bcfe929acc3a1efc09c
r14minji <rkdalswl0728@gmail.com>
2022-01-12 08:42:45 +0900

    main story 스와이퍼 연결

--- js/main.js
+++ js/main.js
@@ -49,3 +49,19 @@ gnb_lis.forEach( li=>{
   }
 })
 
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
