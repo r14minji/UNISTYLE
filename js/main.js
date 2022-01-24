b9e859689df4497c865f3aa8abb211de01b1fc14
r14minji <rkdalswl0728@gmail.com>
2022-01-24 20:05:46 +0900

    쿠키팝업 적용

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ const menuMo = document.querySelector(".menuMo");
 const gnb_lis = document.querySelectorAll(".gnbWrap ul>li");
 const story_check = document.querySelector(".myStory");
 const theViews_check = document.querySelector(".cards");
+const popup = document.querySelector("#popup");
+const btnClose = popup.querySelector(".close");
+const isCookie = document.cookie.indexOf("popup=done");
 
 
 //반응형 메뉴버튼
@@ -81,4 +84,34 @@ if(theViews_check !== null){
     effect: "cards",
     grabCursor: true,
   });
+}
+
+//쿠키가 없다면
+if(isCookie == -1){
+  //console.log("쿠키 없음");
+  popup.style.display = "block";
+} else {
+  //console.log("쿠키 있음");
+  popup.style.display = "none";
+}
+
+//쿠키팝업
+btnClose.addEventListener("click", e => {
+  e.preventDefault();
+
+  let isChecked = popup.querySelector("input[type=checkbox]").checked;
+  let id_name = popup.getAttribute("id");
+
+  if(isChecked) setCookie(id_name, "done", 1);
+  popup.style.display = "none";
+})
+
+//쿠키 생성함수 정의
+function setCookie(cookieName, cookieValue, time){
+  const today = new Date();
+  const date = today.getDate();
+  today.setDate(date + time);
+  const duedate = today.toGMTString(); 
+
+  document.cookie = `${cookieName} = ${cookieValue}; path ="/"; expires=${duedate}`;
 }
